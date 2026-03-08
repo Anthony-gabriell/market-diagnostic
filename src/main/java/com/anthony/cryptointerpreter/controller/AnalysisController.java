fb60a16cb89abdb881f1568e077830abdd3ae08b
Gabriel Anthony <nsasolutions@gmail.com>
2026-03-08 19:39:56 -0300

    Fix: CrossOrigin adicionado com sucesso

--- src/main/java/com/anthony/cryptointerpreter/controller/AnalysisController.java
+++ src/main/java/com/anthony/cryptointerpreter/controller/AnalysisController.java
@@ -7,11 +7,15 @@ import com.anthony.cryptointerpreter.dto.DiagnosticReportDTO;
 import com.anthony.cryptointerpreter.dto.TopOpportunityDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/analysis")
 @RequiredArgsConstructor
